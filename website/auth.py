87fa1b77dbf6b78c7256d44e7f59c34d31397fa5
Oskar Hosken <ohos0003@student.monash.edu>
2024-06-18 18:17:51 +1000

    basic sign up sheet added

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,10 @@ User Authentication.
 """
 import os
 from dotenv import load_dotenv
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 load_dotenv()
 
@@ -11,4 +14,5 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/signup", methods=["GET", "POST"])
 def sign_up():
-    return render_template("signup.html")
+
+    return render_template("signup.html", user=current_user)
