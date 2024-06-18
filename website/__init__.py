21126c76238903e4fca8d108ea7bde04aa688548
Oskar Hosken <ohos0003@student.monash.edu>
2024-06-18 12:24:26 +1000

    Fleshing out project

---
+++ website/__init__.py
@@ -0,0 +1,31 @@
+"""
+Let's begin by initialising our app so we can do stuff with it.
+"""
+# Importing stuff I think will be important
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+
+# Setting up our database
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+# For env variables
+load_dotenv()
+
+# And now creating our app
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    # Using SQLite for local projects
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+
+    # No background stuff going on
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    # Going to leave it here. Let's get a base html page up.
